5f4df7965e043340084902a6a7c68728be0b73ac
elchincode <elchinai@code.edu.az>
2023-01-22 20:43:10 +0400

    HomeWork

---
+++ Two-dimensional array/Program.cs
@@ -0,0 +1,159 @@
+﻿using System;
+
+namespace Two_dimensional_array
+
+{
+    class HelloWorld
+    {
+        static void Main(String[] args)
+        {
+
+
+
+
+
+
+
+
+
+            //////////111111111111111111111
+
+            //int[,] arrr = new int[3, 3]
+            //{
+            //    {1,2,3},
+            //    {5,6,7},
+            //    {1,10,22},
+
+
+            //};
+
+            //int total = 0;
+
+
+
+
+            //for (int i = 0; i < arrr.GetLength(0); i++) //// row
+            //{
+            //    for (int y = 0; y < arrr.GetLength(1); y++) ///// calumn
+            //    {
+            //        if (i == y)
+            //        {
+            //            total += arrr[i, y];
+
+            //        }
+
+
+            //    }
+
+
+
+            //}
+            //Console.Write(total);
+
+
+
+
+
+            ///////////222222222222222
+
+
+            //int[,] arrr = new int[5, 5]
+            //{
+            //    {1,1,1,1,1},
+            //    {1,1,1,1,1},
+            //    {1,1,1,1,1},
+            //    {1,1,1,1,1},
+            //    {1,1,1,1,1}
+
+
+
+            //};
+
+            //   int[,] arrr1 = new int[5, 5]
+            //{
+            //    {1,1,1,1,1},
+            //    {1,1,1,1,1},
+            //    {1,1,1,1,1},
+            //    {1,1,1,1,1},
+            //    {1,1,1,1,1}
+
+
+
+            //};
+
+            //int[,] arrr2 = new int[5, 5];
+
+
+
+
+
+
+
+            //for (int i = 0; i < arrr2.GetLength(0); i++)
+            //{
+            //    for (int y = 0; y < arrr2.GetLength(1); y++)
+            //    {
+
+
+            //        arrr2[i, y] = arrr1[i, y] + arrr[i, y];
+            //        Console.Write(arrr2[i,y] + " ");
+
+            //    }
+            //    Console.WriteLine();
+
+            //}
+
+
+
+            //////////////////33333333333333333333333
+
+
+
+
+            //int[,] arrr = new int[5, 5]
+            //{
+            //    {1,1,1,1,1},
+            //    {1,1,1,1,1},
+            //    {1,1,1,1,1},
+            //    {1,1,1,1,1},
+            //    {1,1,1,1,1}
+
+
+
+            //};
+
+
+
+
+            //for (int i = 0; i < arrr.GetLength(0); i++)
+            //{
+            //    for (int y = 0; y < arrr.GetLength(1); y++)
+            //    {
+            //        if (i + y >= 5)
+            //        {
+            //            arrr[i, y] = 0;
+
+
+            //        }
+
+            //        Console.Write(arrr[i, y] + " ");
+
+
+            //    }
+            //    Console.WriteLine();
+
+            //}
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
